3bb393a5bc2da6be8f3f31b425d486cb976d69ff
Sutirtha Nath <87424205+Sutirtha2000@users.noreply.github.com>
2024-07-07 18:07:38 +0530

    Add files via upload

---
+++ source.js
@@ -0,0 +1,17 @@
+const openBtn = document.getElementById("open-btn");
+const modalContainer = document.getElementById("modal-container");
+const closeBtn = document.getElementById("close-btn");
+
+openBtn.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
